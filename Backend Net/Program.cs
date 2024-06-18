9cfb192e6b4b7eb63b764ec5354cfbe8dddef65d
Cristian Pérez <thequintillo@gmail.com>
2024-06-18 19:29:01 +0200

    Integración de Doppler tanto en Auth como Firestore junto a Firestore en general y difrentes profiles para ejecutar la api con doppler

--- Backend Net/Program.cs
+++ Backend Net/Program.cs
@@ -1,61 +1,104 @@
-using FirebaseAdmin.Auth;
+using Backend_Net.Interfaces;
+using Backend_Net.Services;
 using FirebaseAdmin;
+using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.IO;
-//prueba de git
+using System.Text.Json;
+
+
+
 IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 
-// Configuración de Firebase Admin
+
+string type = Environment.GetEnvironmentVariable("TYPE");
+string projectId = Environment.GetEnvironmentVariable("PROJECT_ID");
+string privateKeyId = Environment.GetEnvironmentVariable("PRIVATE_KEY_ID");
+string privateKey = Environment.GetEnvironmentVariable("PRIVATE_KEY");
+string clientEmail = Environment.GetEnvironmentVariable("CLIENT_EMAIL");
+string clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+string authUri = Environment.GetEnvironmentVariable("AUTH_URI");
+string tokenUri = Environment.GetEnvironmentVariable("TOKEN_URI");
+string authProviderCertUrl = Environment.GetEnvironmentVariable("AUTH_PROVIDER_CERT_URL");
+string clientCertUrl = Environment.GetEnvironmentVariable("CLIENT_CERT_URL");
+string universeDomain = Environment.GetEnvironmentVariable("UNIVERSE_DOMAIN");
+string GOOGLE_APPLICATION = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION");
+object firebaseConfig = new
+{
+    type,
+    project_id = projectId,
+    private_key_id = privateKeyId,
+    private_key = privateKey.Replace("\\n", "\n"),
+    client_email = clientEmail,
+    client_id = clientId,
+    auth_uri = authUri,
+    token_uri = tokenUri,
+    auth_provider_x509_cert_url = authProviderCertUrl,
+    client_x509_cert_url = clientCertUrl,
+    universe_domain = universeDomain
+};
+
+string json = JsonSerializer.Serialize(firebaseConfig);
+
+
+string tempFilePath = Path.GetTempFileName();
+string tempJsonFilePath = Path.ChangeExtension(tempFilePath, ".json");
+
+// Escribir el contenido JSON en el archivo temporal
+File.WriteAllText(tempJsonFilePath, GOOGLE_APPLICATION);
+
+// Establecer la variable de entorno GOOGLE_APPLICATION_CREDENTIALS con la ruta del archivo temporal
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", tempJsonFilePath);
+
+// Ejemplo de lectura de la variable de entorno configurada
+string GOOGLE_APPLICATION_CREDENTIALS = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+
+
+GoogleCredential credential;
+using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+{
+    credential = GoogleCredential.FromStream(stream);
+}
 FirebaseApp.Create(new AppOptions()
 {
-    Credential = GoogleCredential.GetApplicationDefault(),
-    ProjectId = "backendnet-c442c",
+    Credential = credential,
+    ProjectId = projectId,
 });
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Agregar servicios al contenedor
-builder.Services.AddRazorPages();
+builder.Services.AddScoped<IFirestoreService, FirestoreService>();
 
-// Registrar servicios Firestore y otros servicios necesarios
 builder.Services.AddSingleton<FirestoreDb>(provider =>
 {
-    var projectId = configuration["backendnet-c442c"];
+    var projectId = Environment.GetEnvironmentVariable("PROJECT_ID");
     return FirestoreDb.Create(projectId);
 });
 
-// Configurar Firebase Authentication
 builder.Services.AddSingleton<FirebaseAuth>(provider =>
 {
     return FirebaseAuth.DefaultInstance;
 });
+builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
-// Middleware y configuraciones de la aplicación
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
-// Endpoints
 app.MapRazorPages();
 app.MapControllers();
 
-// Ejemplo de endpoint con Firebase Firestore
-/*app.MapGet("/users", async (FirestoreDb db) =>
-{
-    var usersCollection = db.Collection("users");
-    var query = await usersCollection.GetSnapshotAsync();
-    var users = query.Documents.Select(doc => doc.ConvertTo<User()).ToList();
-
-    return Results.Ok(users);
-});*/
+Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Program>();
+                webBuilder.UseUrls("https://localhost:7298", "http://localhost:5007");
+            });
 
 app.Run();
--- Backend Net/wwwroot/js/authGoogle.js
+++ Backend Net/wwwroot/js/authGoogle.js
@@ -3,26 +3,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Configuración de Firebase
-const firebaseConfig = {
-
-    apiKey: "AIzaSyDmiAT-6KqKyNCX6b-T6y7TQwV5gU0jjO0",
-
-    authDomain: "backendnet-c442c.firebaseapp.com",
-
-    projectId: "backendnet-c442c",
-
-    storageBucket: "backendnet-c442c.appspot.com",
-
-    messagingSenderId: "296296323514",
-
-    appId: "1:296296323514:web:596bf5a9dda9b9a5f76583",
-
-    measurementId: "G-RTWE5NG2YZ"
-
-};
-
-
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
